066c073cb753d732c5a252d7c632e2f24f562950
amanuela97 <babyzewdie@gmail.com>
2022-07-30 23:00:16 +0300

    feat: icon and remove debug

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -13,7 +13,7 @@ i18next
   .use(initReactI18next)
   .use(LanguageDetector)
   .init({
-    debug: true,
+    debug: false,
     fallbackLng: "en",
     ns: ["translations"],
     defaultNS: "translations",
